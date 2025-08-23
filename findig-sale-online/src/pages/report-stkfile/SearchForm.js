742b4da6a99a49e68f000d2584533eb8e7e02457
นที ภูวงษ <natee.live@gmail.com>
2025-08-23 21:50:27 +0700

    add zIndex for SearchForm

--- findig-sale-online/src/pages/report-stkfile/SearchForm.js
+++ findig-sale-online/src/pages/report-stkfile/SearchForm.js
@@ -49,6 +49,7 @@ const SearchForm = ({
         "cardBorder",
         currentTheme
       )} animate-fade-in`}
+      style={{ zIndex: 9999, position: "relative" }}
     >
       <div className="p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-4">
