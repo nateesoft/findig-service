89a44827fca3bc867089f7045c97f66c74c608c5
nateelive <natee.live@gmail.com>
2026-05-15 14:06:44 +0700

    update setupProxy invalid fixed bug

--- findig-sale-online/src/setupProxy.js
+++ findig-sale-online/src/setupProxy.js
@@ -4,10 +4,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     const backendHost = process.env.BACKEND_HOST || 'http://127.0.0.1:9090';
     app.use(
-        '/api/findig-backend-service',
         createProxyMiddleware({
             target: backendHost,
             changeOrigin: true,
+            pathFilter: '/api/findig-backend-service',
         })
     );
 };
